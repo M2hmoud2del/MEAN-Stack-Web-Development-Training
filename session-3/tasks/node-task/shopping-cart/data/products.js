d7cc78f560413cbf12e69aeafe541655798ecdc2
Mahmoud Adel <86129182+M2hmoud2del@users.noreply.github.com>
2026-06-30 15:16:07 +0300

    Add products data for shopping cart

---
+++ session-3/tasks/node-task/shopping-cart/data/products.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        id: 1,
+        name: "Laptop",
+        price: 30000
+    },
+    {
+        id: 2,
+        name: "Mouse",
+        price: 500
+    },
+    {
+        id: 3,
+        name: "Keyboard",
+        price: 1200
+    },
+    {
+        id: 4,
+        name: "Monitor",
+        price: 7000
+    },
+    {
+        id: 5,
+        name: "Headphones",
+        price: 1800
+    }
+];
+
+module.exports = products;
