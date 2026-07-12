45fe68bc7b464eae7dedc0f42ec59de0e674d8e1
Mahmoud Adel <mahmoud.adel0221@gmail.com>
2026-07-12 17:24:45 +0300

    refactor: move landing page components to a separate module

--- session-11/nti-app-module/src/app/app-module.ts
+++ session-11/nti-app-module/src/app/app-module.ts
@@ -2,14 +2,12 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { App } from './app';
-import { FooterComponent } from './components/footer/footer.component';
-import { FeaturesComponent } from './components/features/features.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingPageModule } from './public/landing-page/landing-page-module';
+
 
 @NgModule({
-  declarations: [App, NavbarComponent, HeroComponent, FeaturesComponent, FooterComponent],
-  imports: [BrowserModule],
+  declarations: [App],
+  imports: [BrowserModule, LandingPageModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
